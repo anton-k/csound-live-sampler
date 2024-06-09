813d774e046b9cca7d9a9615851438aacd8391ff
anton-k <anton.kholomiov@gmail.com>
2024-06-09 14:43:42 +0300

    Defains types for audio outputs

--- src/Live/Scene/Audio/Config.hs
+++ src/Live/Scene/Audio/Config.hs
@@ -3,6 +3,9 @@ module Live.Scene.Audio.Config
   , AudioInputConfig (..)
   , MonoInputConfig (..)
   , StereoInputConfig (..)
+  , AudioOutputConfig (..)
+  , MonoOutputConfig (..)
+  , StereoOutputConfig (..)
   ) where
 
 import Data.Text (Text)
@@ -12,11 +15,12 @@ import Data.Default
 data AudioConfig channel = AudioConfig
   { csound :: Maybe Text
   , inputs :: Maybe [AudioInputConfig channel]
+  , outputs :: Maybe [AudioOutputConfig channel]
   }
   deriving (Functor)
 
 instance Default (AudioConfig a) where
-  def = AudioConfig { csound = Nothing, inputs = Nothing }
+  def = AudioConfig { csound = Nothing, inputs = Nothing, outputs = Nothing }
 
 data AudioInputConfig channel
   = StereoAudioInputConfig (StereoInputConfig channel)
@@ -39,7 +43,29 @@ data MonoInputConfig channel = MonoInputConfig
   }
   deriving (Functor)
 
+data AudioOutputConfig channel
+  = StereoAudioOutputConfig (StereoOutputConfig channel)
+  | MonoAudioOutputConfig (MonoOutputConfig channel)
+  deriving (Functor)
+
+data StereoOutputConfig channel = StereoOutputConfig
+  { channel :: Maybe channel
+  , gain :: Maybe Float
+  , stereo :: (Int, Int)
+  }
+  deriving (Functor)
+
+data MonoOutputConfig channel = MonoOutputConfig
+  { channel :: Maybe channel
+  , gain :: Maybe Float
+  , mono :: Int
+  }
+  deriving (Functor)
+
 $(Json.deriveJSON Json.defaultOptions ''MonoInputConfig)
 $(Json.deriveJSON Json.defaultOptions ''StereoInputConfig)
 $(Json.deriveJSON Json.defaultOptions { Json.sumEncoding = Json.UntaggedValue } ''AudioInputConfig)
+$(Json.deriveJSON Json.defaultOptions ''MonoOutputConfig)
+$(Json.deriveJSON Json.defaultOptions ''StereoOutputConfig)
+$(Json.deriveJSON Json.defaultOptions { Json.sumEncoding = Json.UntaggedValue } ''AudioOutputConfig)
 $(Json.deriveJSON Json.defaultOptions ''AudioConfig)
